d105665b29e863e462b7b4660ce4f87e6d834888
Jordi Petit <jordi.petit@gmail.com>
2019-03-25 09:23:38 +0100

    Millores

--- bot2.py
+++ bot2.py
@@ -6,10 +6,10 @@ from telegram.ext import CommandHandler
 
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Hola! Soc un bot amb comandes /info i /hora.")
+    bot.send_message(chat_id=update.message.chat_id, text="Hola!")
 
-def info(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="El meu autor és en Jordi")
+def help(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Soc un bot amb comandes /start, /help i /hora.")
 
 def hora(bot, update):
     missatge = str(datetime.datetime.now())
@@ -21,7 +21,7 @@ updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('info', info))
+dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(CommandHandler('hora', hora))
 
 updater.start_polling()
